d079b5a20a84a1e7cc697698bee7a63a683706ab
tirth-bhagwat <tirth.bhagwat@proton.me>
2024-07-15 21:22:06 +0530

    add queries at tests/SQL

--- tests/SQL/count_grade_A_assignments_by_teacher_with_max_grading.sql
+++ tests/SQL/count_grade_A_assignments_by_teacher_with_max_grading.sql
@@ -1,1 +1,7 @@
 -- Write query to find the number of grade A's given by the teacher who has graded the most assignments
+
+select count(grade) from assignments as a
+	where grade='A'
+    group by teacher_id
+    order by count(grade) desc 
+    limit 1;
--- tests/SQL/number_of_graded_assignments_for_each_student.sql
+++ tests/SQL/number_of_graded_assignments_for_each_student.sql
@@ -1,1 +1,6 @@
 -- Write query to get number of graded assignments for each student:
+
+
+select a.student_id as ID, count(state) as assignments from assignments as a
+	where state='GRADED'
+    group by a.student_id;
